0c7f0e40b38a30cc2108f722cb0067b965a08597
Beronica Ocasio <28956655+beronicao@users.noreply.github.com>
2024-05-29 16:33:03 -0400

    update MLP model R script

--- scripts/R/SS_MLP.R
+++ scripts/R/SS_MLP.R
@@ -23,9 +23,9 @@ set.seed(n_seed)
 tensorflow::set_random_seed(seed = n_seed)
 
 # Define Model Architecture:
-input_A <- layer_input(shape = c(969), dtype = 'float32', name = 'input_A')
+input_A_MLP <- layer_input(shape = c(969), dtype = 'float32', name = 'input_A_MLP')
 
-out_A <- input_A %>%
+out_A_MLP <- input_A_MLP %>%
   layer_dense(units = 969, activation = 'relu', input_shape = c(969)) %>%
   layer_dense(units = 512, activation = 'relu') %>%
   layer_dense(units = 256, activation = 'relu') %>%
@@ -33,9 +33,9 @@ out_A <- input_A %>%
   layer_dense(units = 64, activation = 'relu') %>%
   layer_dense(units = 32, activation = 'linear')
 
-input_B <- layer_input(shape = c(969), dtype = 'float32', name = 'input_B')
+input_B_MLP <- layer_input(shape = c(969), dtype = 'float32', name = 'input_B_MLP')
 
-out_B <- input_B %>% 
+out_B_MLP <- input_B_MLP %>% 
   layer_dense(units = 969, activation = 'relu', input_shape = c(969)) %>%
   layer_dense(units = 512, activation = 'relu') %>%
   layer_dense(units = 256, activation = 'relu') %>%
@@ -43,37 +43,37 @@ out_B <- input_B %>%
   layer_dense(units = 64, activation = 'relu') %>%
   layer_dense(units = 32, activation = 'linear')
 
-output <- layer_concatenate(c(out_A, out_B)) %>%
+output_MLP <- layer_concatenate(c(out_A_MLP, out_B_MLP)) %>%
   layer_dense(units = 64, activation = 'relu') %>%
   layer_dropout(rate = 0.15) %>%
   layer_dense(units = 64, activation = 'relu') %>%
   layer_dropout(rate = 0.15) %>%
   layer_dense(units = 64, activation = 'relu') %>%
   layer_dropout(rate = 0.15) %>%
-  layer_dense(units = 1, activation = 'sigmoid', name = 'output')
+  layer_dense(units = 1, activation = 'sigmoid', name = 'output_MLP')
 
-SS_model <- keras_model(
-  inputs = c(input_A, input_B),
-  outputs = c(output)
+SSeq_model <- keras_model(
+  inputs = c(input_A_MLP, input_B_MLP),
+  outputs = c(output_MLP)
 )
 
-SS_model %>% compile(
+SSeq_model %>% compile(
   optimizer = 'adam',
   loss = list(output = 'binary_crossentropy'),
   loss_weights = list(output = 1.0),
   metrics = c('binary_accuracy')
 )
 
-# summary(SS_model) # print summary of model (optional)
+# summary(SSeq_model) # print summary of model (optional)
 
-history.fit <- SS_model %>% fit(
-  x = list(input_A = train_A, input_B = train_B),
+history.fit <- SSeq_model %>% fit(
+  x = list(input_A_MLP = train_A, input_B_MLP = train_B),
   y = train_labels[,2], 
   epochs = n_epochs, 
   # class_weight = class_weights, 
   batch_size = n_batch_size,
   validation_data = list(
-    x = list(input_A = val_A, input_B = val_B), 
+    x = list(input_A_MLP = val_A, input_B_MLP = val_B), 
     y = val_labels[,2]
   )
 )
@@ -81,22 +81,22 @@ history.fit <- SS_model %>% fit(
 # plot(history.fit). # plot model training and validation (optional)
 
 ## Save model: #
-save_model_hdf5(SS_model, "SS_MLP_model_hdf5.h5")
+save_model_hdf5(SSeq_model, "SSeq_MLP_model_hdf5.h5")
 
 ## Save model weights: #
-save_model_weights_hdf5(SS_model, "SS_MLP_model_weights_hdf5.h5") 
+save_model_weights_hdf5(SSeq_model, "SSeq_MLP_model_weights_hdf5.h5") 
 
 ## Load pre-trained model: #
-# SS_model <- load_model_hdf5("SS_MLP_model_hdf5.h5")
-# SS_model <- load_model_weights_hdf5(SS_model, "SS_MLP_model_weights_hdf5.h5")
+# SSeq_model <- load_model_hdf5("SSeq_MLP_model_hdf5.h5")
+# SSeq_model <- load_model_weights_hdf5(SSeq_model, "SSeq_MLP_model_weights_hdf5.h5")
 
 ## Generate predictions: #
-predict <- SS_model %>% predict(
-  x = list(input_A = test_A, input_B = test_B),
+predict <- SSeq_model %>% predict(
+  x = list(input_A_MLP = test_A, input_B_MLP = test_B),
   batch_size = n_batch_size)
 
 ## Evaluate predictions: #
-score <- SS_model %>% evaluate(
-  x = list(input_A = test_A, input_B = test_B),
+score <- SSeq_model %>% evaluate(
+  x = list(input_A_MLP = test_A, input_B_MLP = test_B),
   y = test_labels[,2],
   batch_size = n_batch_size)
